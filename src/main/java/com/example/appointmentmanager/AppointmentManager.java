818b0a98978df0a31e3c08c8b4592c16438173ce
alexgvoz <32687836+alexgvoz@users.noreply.github.com>
2022-07-13 19:31:26 -0700

    Start of login page

--- src/main/java/com/example/appointmentmanager/AppointmentManager.java
+++ src/main/java/com/example/appointmentmanager/AppointmentManager.java
@@ -1,5 +1,6 @@
 package com.example.appointmentmanager;
 
+import helper.JDBC;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -12,13 +13,13 @@ public class AppointmentManager extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AppointmentManager.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Appointment Scheduler");
+        stage.setTitle("Appointment Manager");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        //JDBC.openConnection();
+        JDBC.openConnection();
         launch();
     }
 }
--- src/main/java/com/example/appointmentmanager/LoginController.java
+++ src/main/java/com/example/appointmentmanager/LoginController.java
@@ -1,7 +1,11 @@
 package com.example.appointmentmanager;
 
+import helper.JDBC;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class LoginController {
     @FXML
@@ -9,6 +13,18 @@ public class LoginController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        try {
+            PreparedStatement prepstatement = JDBC.connection.prepareStatement("SELECT * FROM users");
+            ResultSet resset = prepstatement.executeQuery();
+            System.out.println(resset.getString("User_Name"));
+
+        } catch(Exception e) {
+            System.out.println("Ooops didnt work");
+        }
+    }
+
+    @FXML
+    public void initialize() {
+
     }
 }
